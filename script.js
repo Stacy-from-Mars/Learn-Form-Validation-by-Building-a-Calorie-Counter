78249ada774c774e863802be1edae2711d8d1153
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-03-27 21:29:39 +0300

    step 22

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ let isError = false;
 function cleanInputString(str) {
     const strArray = str.split('');
     const cleanStrArray = [];
-    for (let i = 0; i < strArray.length; i++) {
 
+    for (let i = 0; i < strArray.length; i++) {
+        if(!["+", "-", " "].includes(strArray[i])) {
+            cleanStrArray.push(strArray[i])
+        }
     }
 }
