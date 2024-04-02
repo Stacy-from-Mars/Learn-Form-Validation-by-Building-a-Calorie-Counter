bec04d8ad175cf89dae8b030ca491283dad7ade4
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-04-02 23:23:46 +0300

    step 54

--- script.js
+++ script.js
@@ -27,5 +27,8 @@ function addEntry() {
     `;
     targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+function getCaloriesFromInputs(list) {
+
+}
 
 addEntryButton.addEventListener("click", addEntry);
